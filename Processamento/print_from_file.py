9423fabaab0c7291a1d2881f90390d533c75f2fc
root <root@DESKTOP-LU0Q8UT.localdomain>
2022-01-16 16:22:31 -0300

    Print from file com processamento de cor e junção de partes da peça

--- Processamento/print_from_file.py
+++ Processamento/print_from_file.py
@@ -55,6 +55,41 @@ def limpa_tela():
         bpy.ops.object.mode_set(mode='OBJECT')
         bpy.ops.object.select_all(action='SELECT')
         bpy.ops.object.delete(use_global=False)
+
+def junta_objetos():
+    if bpy.ops.object.mode_set.poll():
+        bpy.ops.object.mode_set(mode='OBJECT')
+        bpy.ops.object.select_all(action='SELECT')
+        bpy.ops.object.join()
+
+#Função tirada de https://vividfax.github.io/2021/01/14/blender-materials.html
+def newMaterial(name_of_material):
+    mat = bpy.data.materials.get(name_of_material)
+    
+    if(mat) is None:
+        mat = bpy.data.materials.new(name=name_of_material)
+    mat.use_nodes = True
+    
+    if mat.node_tree:
+        mat.node_tree.links.clear()
+        mat.node_tree.nodes.clear()
+    return mat
+
+def newShader(name_of_material, r, g, b):
+    mat = newMaterial(name_of_material)
+    
+    nodes = mat.node_tree.nodes
+    links = mat.node_tree.links
+    
+    output = nodes.new(type='ShaderNodeOutputMaterial')
+
+    shader = nodes.new(type='ShaderNodeBsdfGlossy')
+    nodes["BSDF - Polimento"].inputs[0].default_value = (r, g, b, 1)
+    nodes["BSDF - Polimento"].inputs[1].default_value = 0
+
+    links.new(shader.outputs[0], output.inputs[0])
+    
+    return mat
         
 
 if __name__ == '__main__':
@@ -62,3 +97,6 @@ if __name__ == '__main__':
     #desenha_bolinhas(m)
     desenha_circulo_2D(m)
     #desenha_quadrado_2D(m)
+    junta_objetos()
+    #mat = newShader("Shader", 0.494,0.894, 0.658)
+    mat = newShader("Shader2", 0.658, 0.428, 0.038)
